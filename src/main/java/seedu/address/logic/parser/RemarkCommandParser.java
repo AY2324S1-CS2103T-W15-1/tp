3b344593c3fa37ce57fd18bed100dc8c9c644cd8
kiatkat <kiatkat123@gmail.com>
2023-09-30 17:47:17 +0800

    Enhance remark
    Prior addition of remark was just a placeholder command. Add parsing
    capability to remark.`

---
+++ src/main/java/seedu/address/logic/parser/RemarkCommandParser.java
@@ -0,0 +1,34 @@
+package seedu.address.logic.parser;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+
+import seedu.address.commons.core.index.Index;
+import seedu.address.commons.exceptions.IllegalValueException;
+import seedu.address.logic.commands.Command;
+import seedu.address.logic.commands.RemarkCommand;
+import seedu.address.logic.parser.exceptions.ParseException;
+
+/**
+ * Parses input arguments and creates a new RemarkCommand object
+ */
+public class RemarkCommandParser implements Parser {
+    private static final Prefix PREFIX_REMARK = new Prefix("r/");
+    @Override
+    public Command parse(String userInput) throws ParseException {
+        requireNonNull(userInput);
+        ArgumentMultimap argumentMultimap = ArgumentTokenizer.tokenize(userInput, PREFIX_REMARK);
+
+        Index index;
+        try {
+            index = ParserUtil.parseIndex(argumentMultimap.getPreamble());
+        } catch (IllegalValueException ive) {
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT,
+                    RemarkCommand.MESSAGE_USAGE), ive);
+        }
+
+        String remark = argumentMultimap.getValue(PREFIX_REMARK).orElse("");
+
+        return new RemarkCommand(index, remark);
+    }
+}
